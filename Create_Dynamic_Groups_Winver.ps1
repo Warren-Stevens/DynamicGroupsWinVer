56bd5a67a6e89b22e7dfb99277b4e39aff4a73dc
Warren-Stevens <34251334+Warren-Stevens@users.noreply.github.com>
2023-11-14 12:40:04 +0200

    Update Create_Dynamic_Groups_Winver.ps1
    1. Added variable for naming standard.
    2. Added check if group exists.

--- Create_Dynamic_Groups_Winver.ps1
+++ Create_Dynamic_Groups_Winver.ps1
@@ -1,12 +1,10 @@
-﻿# Create a Dynamic Group for each Windows Version, including groups for All Windows 10 and 11 
+# Create a Dynamic Group for each Windows Version, including groups for All Windows 10 and 11 
 
 # Check if AzureAD module is installed - run PS with admin privileges 
 if (Get-Module -Name AzureAD -ListAvailable) {
     # Uninstall AzureAD module
     Uninstall-Module -Name AzureAD -AllVersions -Force
 }
-
-# Refresh Powershell - close and open PS again with admin privileges  
 
 # Install and Import Azure AD Preview
 Install-Module -Name AzureADPreview -AllowClobber -Confirm -force
@@ -15,37 +13,43 @@ Import-Module Azureadpreview
 # Connect to Azure AD
 Connect-AzureAD
 
-# Create Dynamic Groups
+# Create Dynamic Groups (CHANGE "TEST")
+$CompanyName = "Test"
 
-# Windows 10 version 1903 (build 18362)
-New-AzureADMSGroup -DisplayName "SG | Windows 10 | Build 1903" -Description "Containing all Windows 10 version 1903" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.18362") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
+# Function to check if a group exists
+function GroupExists($groupName) {
+    $existingGroup = Get-AzureADMSGroup -Filter "displayName eq '$groupName'"
+    if ($existingGroup) {
+        return $true
+    }
+    return $false
+}
 
-# Windows 10 version 1909 (build 18363)
-New-AzureADMSGroup -DisplayName "SG | Windows 10 | Build 1909" -Description "Containing all Windows 10 version 1909" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.18363") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
+# Function to create a group if it doesn't exist
+function CreateGroup($displayName, $description, $membershipRule) {
+    if (-not (GroupExists $displayName)) {
+        New-AzureADMSGroup -DisplayName $displayName -Description $description -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule $membershipRule -MembershipRuleProcessingState "On"
+        Write-Host "Group '$displayName' created."
+    } else {
+        Write-Host "Group '$displayName' already exists."
+    }
+}
 
-# Windows 10 version 2004 (build 19041)
-New-AzureADMSGroup -DisplayName "SG | Windows 10 | Build 2004" -Description "Containing all Windows 10 version 2004" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.19041") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
+# Create Windows 10 version groups
+CreateGroup "All $CompanyName | Windows 10 | Build 1909" "Containing all Windows 10 version 1909" '(device.deviceOSVersion -startsWith "10.0.18363") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")'
+CreateGroup "All $CompanyName | Windows 10 | Build 2004" "Containing all Windows 10 version 2004" '(device.deviceOSVersion -startsWith "10.0.19041") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")'
+CreateGroup "All $CompanyName | Windows 10 | Build 20H2" "Containing all Windows 10 version 20H2" '(device.deviceOSVersion -startsWith "10.0.19042") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")'
+CreateGroup "All $CompanyName | Windows 10 | Build 21H1" "Containing all Windows 10 version 21H1" '(device.deviceOSVersion -startsWith "10.0.19043") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")'
+CreateGroup "All $CompanyName | Windows 10 | Build 21H2" "Containing all Windows 10 version 21H2" '(device.deviceOSVersion -startsWith "10.0.19044") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")'
+CreateGroup "All $CompanyName | Windows 10 | Build 22H2" "Containing all Windows 10 version 22H2" '(device.deviceOSVersion -startsWith "10.0.19045") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")'
 
-# Windows 10 version 20H2 (build 19042)
-New-AzureADMSGroup -DisplayName "SG | Windows 10 | Build 20H2" -Description "Containing all Windows 10 version 20H2" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.19042") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
+# Create Windows 11 version groups
+CreateGroup "All $CompanyName | Windows 11 | Build 21H2" "Containing all Windows 11 version 21H2" '(device.deviceOSVersion -startsWith "10.0.22000") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")'
+CreateGroup "All $CompanyName | Windows 11 | Build 22H2" "Containing all Windows 11 version 22H2" '(device.deviceOSVersion -startsWith "10.0.22621") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")'
+CreateGroup "All $CompanyName | Windows 11 | Build 23H2" "Containing all Windows 11 version 23H2" '(device.deviceOSVersion -startsWith "10.0.22631") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")'
 
-# Windows 10 version 21H1 (build 19043)
-New-AzureADMSGroup -DisplayName "SG | Windows 10 | Build 21H1" -Description "Containing all Windows 10 version 21H1" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.19043") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
+# Create "All Windows 10 Devices" group
+CreateGroup "All $CompanyName | All Windows 10 Devices" "Containing all Windows 10 versions" '(device.deviceOSVersion -startsWith "10.0.1") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")'
 
-# Windows 10 version 21H2 (build 19044)
-New-AzureADMSGroup -DisplayName "SG | Windows 10 | Build 21H2" -Description "Containing all Windows 10 version 21H2" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.19044") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
-
-# Windows 10 version 22H2 (build 19045)
-New-AzureADMSGroup -DisplayName "SG | Windows 10 | Build 22H2" -Description "Containing all Windows 10 version 22H2" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.19045") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
-
-# Windows 11 version 21H2 (build 22000)
-New-AzureADMSGroup -DisplayName "SG | Windows 11 | Build 21H2" -Description "Containing all Windows 11 version 21H2" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.22000") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
-
-# Windows 11 version 22H2 (build 22621)
-New-AzureADMSGroup -DisplayName "SG | Windows 11 | Build 22H2" -Description "Containing all Windows 11 version 22H2" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.22621") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
-
-# Windows 10 - All versions
-New-AzureADMSGroup -DisplayName "SG | All Windows 10 Devices" -Description "Containing all Windows 10 versions" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.1") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
-
-# Windows 11 - All versions
-New-AzureADMSGroup -DisplayName "SG | All Windows 11 Devices" -Description "Containing all Windows 11 versions" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.2") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
+# Create "All Windows 11 Devices" group
+CreateGroup "All $CompanyName | All Windows 11 Devices" "Containing all Windows 11 versions" '(device.deviceOSVersion -startsWith "10.0.2") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")'
