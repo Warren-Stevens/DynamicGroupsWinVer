3c73863c44424114fe545232402b5d4ba9464f03
Warren-Stevens <34251334+Warren-Stevens@users.noreply.github.com>
2024-06-15 16:02:06 +0200

    Update Create_Dynamic_Groups_Winver.ps1

--- Create_Dynamic_Groups_Winver.ps1
+++ Create_Dynamic_Groups_Winver.ps1
@@ -1,4 +1,4 @@
-# Create a Dynamic Group for each Windows Version
+# Create a Dynamic Group for each Windows Version, including groups for All Windows 10 and 11 
 
 # Check if AzureAD module is installed - run PS with admin privileges 
 if (Get-Module -Name AzureAD -ListAvailable) {
@@ -14,64 +14,40 @@ Import-Module Azureadpreview
 Connect-AzureAD
 Connect-MSGraph
 
-# Create Dynamic Groups --- rename "COMPANY"
-$CompanyName = "COMPANY"
-$groupNames = @(
-    "$CompanyName | Windows 10 | Build 1903",
-    "$CompanyName | Windows 10 | Build 1909",
-    "$CompanyName | Windows 10 | Build 2004",
-    "$CompanyName | Windows 10 | Build 20H2",
-    "$CompanyName | Windows 10 | Build 21H1",
-    "$CompanyName | Windows 10 | Build 21H2",
-    "$CompanyName | Windows 10 | Build 22H2",
-    "$CompanyName | Windows 11 | Build 21H2",
-    "$CompanyName | Windows 11 | Build 22H2",
-    "$CompanyName | Windows 11 | Build 23H2"
-)
+# Create Dynamic Groups
+ 
+# Windows 10 version 1903 (build 18362)
+New-AzureADMSGroup -DisplayName "Intune | Windows 10 | Build 1903" -Description "Containing all Windows 10 version 1903" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.18362") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
+ 
+# Windows 10 version 1909 (build 18363)
+New-AzureADMSGroup -DisplayName "Intune | Windows 10 | Build 1909" -Description "Containing all Windows 10 version 1909" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.18363") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
+ 
+# Windows 10 version 2004 (build 19041)
+New-AzureADMSGroup -DisplayName "Intune | Windows 10 | Build 2004" -Description "Containing all Windows 10 version 2004" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.19041") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
+ 
+# Windows 10 version 20H2 (build 19042)
+New-AzureADMSGroup -DisplayName "Intune | Windows 10 | Build 20H2" -Description "Containing all Windows 10 version 20H2" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.19042") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
+ 
+# Windows 10 version 21H1 (build 19043)
+New-AzureADMSGroup -DisplayName "Intune | Windows 10 | Build 21H1" -Description "Containing all Windows 10 version 21H1" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.19043") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
+ 
+# Windows 10 version 21H2 (build 19044)
+New-AzureADMSGroup -DisplayName "Intune | Windows 10 | Build 21H2" -Description "Containing all Windows 10 version 21H2" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.19044") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
+ 
+# Windows 10 version 22H2 (build 19045)
+New-AzureADMSGroup -DisplayName "Intune | Windows 10 | Build 22H2" -Description "Containing all Windows 10 version 22H2" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.19045") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
+ 
+# Windows 11 version 21H2 (build 22000)
+New-AzureADMSGroup -DisplayName "Intune | Windows 11 | Build 21H2" -Description "Containing all Windows 11 version 21H2" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.22000") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
+ 
+# Windows 11 version 22H2 (build 22621)
+New-AzureADMSGroup -DisplayName "Intune | Windows 11 | Build 22H2" -Description "Containing all Windows 11 version 22H2" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.22621") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
+ 
+# Windows 11 version 22H3 (build 22621)
+New-AzureADMSGroup -DisplayName "Intune | Windows 11 | Build 23H2" -Description "Containing all Windows 11 version 22H3" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.22631") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
 
-foreach ($groupName in $groupNames) {
-    # Check if the group exists
-    $existingGroup = Get-AzureADMSGroup -Filter "DisplayName eq '$groupName'"
-
-    if ($existingGroup) {
-        Write-Host "The group '$groupName' already exists."
-    } else {
-        # Create Dynamic Group
-        switch -wildcard ($groupName) {
-            "*Build 1903" {
-                New-AzureADMSGroup -DisplayName $groupName -Description "Containing all Windows 10 version 1903" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.18362") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
-            }
-            "*Build 1909" {
-                New-AzureADMSGroup -DisplayName $groupName -Description "Containing all Windows 10 version 1909" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.18363") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
-            }
-            "*Build 2004" {
-                New-AzureADMSGroup -DisplayName $groupName -Description "Containing all Windows 10 version 2004" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.19041") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
-            }
-            "*Build 20H2" {
-                New-AzureADMSGroup -DisplayName $groupName -Description "Containing all Windows 10 version 20H2" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.19042") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
-            }
-            "*Build 21H1" {
-                New-AzureADMSGroup -DisplayName $groupName -Description "Containing all Windows 10 version 21H1" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.19043") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
-            }
-            "*Build 21H2" {
-                New-AzureADMSGroup -DisplayName $groupName -Description "Containing all Windows 10 version 21H2" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.19044") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
-            }
-            "*Build 22H2" {
-                New-AzureADMSGroup -DisplayName $groupName -Description "Containing all Windows 10 version 22H2" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.19045") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
-            }
-            "*Build 21H2" {
-                New-AzureADMSGroup -DisplayName $groupName -Description "Containing all Windows 11 version 21H2" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.22000") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
-            }
-            "*Build 22H2" {
-                New-AzureADMSGroup -DisplayName $groupName -Description "Containing all Windows 11 version 22H2" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.22621") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
-            }
-            "*Build 23H2" {
-                New-AzureADMSGroup -DisplayName $groupName -Description "Containing all Windows 11 version 23H2" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.22631") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
-            }
-            default {
-                Write-Host "Invalid group name."
-            }
-        }
-        Write-Host "The group '$groupName' has been created."
-    }
-}
+# Windows 10 - All versions
+New-AzureADMSGroup -DisplayName "Intune | All Windows 10 Devices" -Description "Containing all Windows 10 versions" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.1") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
+ 
+# Windows 11 - All versions
+New-AzureADMSGroup -DisplayName "Intune | All Windows 11 Devices" -Description "Containing all Windows 11 versions" -MailEnabled $False -MailNickName $False -SecurityEnabled $True -GroupTypes @("DynamicMembership") -MembershipRule '(device.deviceOSVersion -startsWith "10.0.2") -and (device.deviceOSType -eq "Windows") -and (device.managementType -eq "MDM")' -MembershipRuleProcessingState "On"
